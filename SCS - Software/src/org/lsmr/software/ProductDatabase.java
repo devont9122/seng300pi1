d3623a824941ebd5dd95e140a64ed442a269511a
BitZer0 <eschoidai@gmail.com>
2022-03-18 04:47:57 -0600

    Created product database class as a singleton

---
+++ SCS - Software/src/org/lsmr/software/ProductDatabase.java
@@ -0,0 +1,43 @@
+package org.lsmr.software;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.lsmr.selfcheckout.Barcode;
+import org.lsmr.selfcheckout.products.BarcodedProduct;
+
+/**
+ * Contains a database of products.
+ */
+public class ProductDatabase {
+	public static ProductDatabase Instance = new ProductDatabase();
+	
+	private Map<Barcode, BarcodedProduct> barcodedProducts;
+	
+	// Currently a singleton, so we shouldn't be able to create any instances of it
+	private ProductDatabase() {
+		barcodedProducts = new HashMap<>();
+	}
+
+	/*
+	 * Registers the given barcoded products into the database.
+	 * 
+	 * @param	products	Array of BarcodedProduct's to register into the database.
+	 */
+	public void RegisterProducts(BarcodedProduct...products) {
+		for(BarcodedProduct product : products) {
+			barcodedProducts.putIfAbsent(product.getBarcode(), product);
+		}
+	}
+	
+	/*
+	 * Looks up a product with the given barcode and returns it.
+	 * 
+	 *  @param 	barcode Barcode of the product to look up.
+	 *  @return			Product that matches the specified barcode or null if the not in the database.
+	 */
+	public void LookupItemViaBarcode(Barcode barcode) {
+		// Handle case where the barcode is invalid/doesn't map to a product?
+		barcodedProducts.get(barcode);
+	}
+}
