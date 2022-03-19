e9560630fcd599e28b3451ff6d81a0b3eaa56117
doowoncho <84430881+doowoncho@users.noreply.github.com>
2022-03-18 22:41:31 -0600

    Created payWithBankNote
    accepts when there is a valid banknote and when there is space for the banknote. Returns the value of money accepted.

---
+++ SCS - Software/src/org/lsmr/software/payWithBankNote.java
@@ -0,0 +1,65 @@
+package org.lsmr.software;
+
+import java.util.Currency;
+
+import org.lsmr.selfcheckout.Banknote;
+import org.lsmr.selfcheckout.devices.AbstractDevice;
+import org.lsmr.selfcheckout.devices.BanknoteValidator;
+import org.lsmr.selfcheckout.devices.DisabledException;
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
+import org.lsmr.selfcheckout.devices.observers.BanknoteValidatorObserver;
+
+public class payWithBankNote {
+
+		public SelfCheckoutStation station;
+		public boolean valid = false;
+		
+		
+		public int accept(Banknote bNote)
+		{
+			if(valid == true && station.banknoteStorage.hasSpace())
+			{
+				
+				return bNote.getValue();
+			}
+			else
+			{
+				return 0;
+			}	
+		}
+		
+		public BanknoteValidatorObserver listener = new BanknoteValidatorObserver() {
+
+			@Override
+			public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void validBanknoteDetected(BanknoteValidator validator, Currency currency, int value) {
+				// TODO Auto-generated method stub
+				valid = true;
+				
+			}
+
+			@Override
+			public void invalidBanknoteDetected(BanknoteValidator validator) {
+				// TODO Auto-generated method stub
+				valid = false;
+			}
+			
+				
+		};
+		
+		
+		
+		
+}
