ccfad0265a9dfee40d316c65750552dd24ad3fe8
carlo-deg <dg.carlodg@gmail.com>
2022-03-20 16:11:38 -0600

    Update to BaggingArea & Testing
    Just in case, I can't connect.

--- SCS - Software/src/org/lsmr/software/PlaceItemInBaggingArea.java
+++ SCS - Software/src/org/lsmr/software/PlaceItemInBaggingArea.java
@@ -1,22 +1,31 @@
 package org.lsmr.software;
 
 import org.lsmr.selfcheckout.Item;
+
 import org.lsmr.selfcheckout.devices.AbstractDevice;
 import org.lsmr.selfcheckout.devices.ElectronicScale;
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+
 import org.lsmr.selfcheckout.devices.OverloadException;
+
 import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
 import org.lsmr.selfcheckout.devices.observers.ElectronicScaleObserver;
+
 import java.util.ArrayList;
 
-public class PlaceItemInBaggingArea implements ElectronicScaleObserver {
+public class PlaceItemInBaggingArea {
 	public SelfCheckoutStation station;
 	public ArrayList<Item> items = new ArrayList<>();
 	public boolean isAdded = false;
 	public boolean isRemoved = false;
 	public boolean isOverloaded = false;
 	
-	
+	//Constructor 
+	public PlaceItemInBaggingArea(SelfCheckoutStation sct) {
+		station = sct;
+		sct.scale.attach(eso);
+	}
+
 	public void addItem(Item anItem) {
 		station.scale.add(anItem);
 		if (isAdded || isOverloaded) {
@@ -36,32 +45,34 @@ public class PlaceItemInBaggingArea implements ElectronicScaleObserver {
 		}
 	}
 
-	@Override
-	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		// TODO Auto-generated method stub
+	public ElectronicScaleObserver eso = new ElectronicScaleObserver() {
+		@Override
+		public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
+			// TODO Auto-generated method stub
 		
-	}
+		}
 
-	@Override
-	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		// TODO Auto-generated method stub
+		@Override
+		public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
+			// TODO Auto-generated method stub
 		
-	}
+		}
 
-	@Override
-	public void weightChanged(ElectronicScale scale, double weightInGrams) {
-		isAdded = true;
-	}
+		@Override
+		public void weightChanged(ElectronicScale scale, double weightInGrams) {
+			isAdded = true;
+		}
 
-	@Override
-	public void overload(ElectronicScale scale) {
-		// TODO Auto-generated method stub
+		@Override
+		public void overload(ElectronicScale scale) {
+			isOverloaded = true;
 		
-	}
+		}
 
-	@Override
-	public void outOfOverload(ElectronicScale scale) {
-		// TODO Auto-generated method stub
+		@Override
+		public void outOfOverload(ElectronicScale scale) {
+			isRemoved = true;
 		
-	}
+		}
+	};
 }
--- SCS - Software/test/org/lsmr/software/PlaceItemInBaggingAreaTest.java
+++ SCS - Software/test/org/lsmr/software/PlaceItemInBaggingAreaTest.java
@@ -3,13 +3,17 @@ package org.lsmr.software;
 import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
+
 import java.util.ArrayList;
 import java.util.Currency;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import org.lsmr.selfcheckout.Barcode;
+import org.lsmr.selfcheckout.BarcodedItem;
 import org.lsmr.selfcheckout.Item;
+import org.lsmr.selfcheckout.Numeral;
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
 import org.lsmr.selfcheckout.devices.observers.ElectronicScaleObserver;
 
@@ -21,14 +25,31 @@ public class PlaceItemInBaggingAreaTest {
 	
 	@Test
 	public void testAddItem() {
-		// Test Creations
+		//Creation of the Self-Checkout Machine & Bagging Area
 		Currency testCurrency = Currency.getInstance("CAD");
 		int[] testBanknoteDenominations = {1, 2, 5};
 		BigDecimal[] testCoinDenominations = {BigDecimal.TEN};
-		SelfCheckoutStation testStation = new SelfCheckoutStation(testCurrency, testBanknoteDenominations, testCoinDenominations, 0, 0);
+		SelfCheckoutStation testStation = new SelfCheckoutStation(testCurrency, testBanknoteDenominations, testCoinDenominations, 50, 2);	// Wasn't sure what the max weight should be (not addressed in Assignment 1 or Project Iteration 1), had to look up: 23kg or 50lbs
+		PlaceItemInBaggingArea testBaggingArea = new PlaceItemInBaggingArea(testStation);
 		
-		PlaceItemInBaggingArea testBagging = new PlaceItemInBaggingArea();
+		//Creation of Item
+		Barcode testBarcode = new Barcode(new Numeral[] {Numeral.five, Numeral.eight, Numeral.three, Numeral.four});
+		Item testItem = new BarcodedItem(testBarcode, 10);
 		
+		//Adding item onto baggage area
+		testBaggingArea.addItem(testItem);
+		
+		//Expected Result
+		ArrayList<Item> expectedList = new ArrayList<>();
+		expectedList.add(testItem);
+		
+		//Actual Result
+		ArrayList<Item> actualList = testBaggingArea.items;
+		assertEquals(actualList, expectedList);
 	}
 
+	@Test
+	public void testAddMultipleItems() {
+		//Creation
+	}
 }
