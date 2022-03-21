e53cb6815f87b5b8d35c8fd18650d3fa0d8ed771
devont9122 <devon.tran@ucalgary.ca>
2022-03-21 00:25:44 -0600

    scrapped the idea of checking bagging area

--- SCS - Software/test/org/lsmr/software/CustomerScansItemTest.java
+++ SCS - Software/test/org/lsmr/software/CustomerScansItemTest.java
@@ -3,6 +3,7 @@ package org.lsmr.software;
 import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
+import java.util.Currency;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -15,6 +16,7 @@ import org.lsmr.selfcheckout.Numeral;
 import org.lsmr.selfcheckout.devices.BarcodeScanner;
 import org.lsmr.selfcheckout.devices.ElectronicScale;
 import org.lsmr.selfcheckout.devices.OverloadException;
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
 import org.lsmr.selfcheckout.products.BarcodedProduct;
 import org.lsmr.selfcheckout.products.Product;
 import org.lsmr.software.ShoppingCart.ShoppingCartEntry;
@@ -25,19 +27,31 @@ class ItemStub extends Item{
 	}
 }
 
+
+
+
 public class CustomerScansItemTest {
-	BarcodeScanner scanner;
+	
 	CustomerScansItem scan;
-	ElectronicScale scale;
+	
 	ProductDatabase database = ProductDatabase.Instance;
 	ShoppingCart cart = ShoppingCart.Instance;
+	Currency currency = Currency.getInstance("CAD");
+	int[] banknoteDenominations = {1, 2, 5, 10};
+	BigDecimal[] coinDenominations = {BigDecimal.TEN};
+	SelfCheckoutStation station = new SelfCheckoutStation(currency, banknoteDenominations, coinDenominations, 10, 2);
 	
 	
 	@Before
 	public void setUp() {
-		scanner = new BarcodeScanner();
-		scan = new CustomerScansItem();
-		scale = new ElectronicScale(10, 2);
+		scan = new CustomerScansItem(station, cart, database);
+		station.scanner.attach(scan);
+		station.scanner.enable();
+		station.scale.enable();
+		
+		cart.Empty();
+		scan.emptyStatus();
+		database.clearDatabase();
 	
 	}
 	
@@ -46,16 +60,17 @@ public class CustomerScansItemTest {
 		Numeral[] n = {Numeral.one, Numeral.two, Numeral.three};
 		Barcode b = new Barcode(n);
 		BarcodedItem item = new BarcodedItem(b, 2.0);
-		scanner.scan(item);
-		if(scan.getScanStatus(b).equals(1)) {
-			assertTrue(scanner.isDisabled());
-		}
+		station.scanner.scan(item);
+	
+		assertTrue(station.scanner.isDisabled());
+	
 		
 		scan.emptyStatus();
 	}
 	
 	@Test
 	public void waitingInterruptedTest() {
+		
 		
 	}
 	
@@ -68,15 +83,16 @@ public class CustomerScansItemTest {
 		BarcodedProduct product = new BarcodedProduct(b, "a product", BigDecimal.valueOf(1.99));
 		database.RegisterProducts(product);
 		
-		scanner.scan(item);
-		scale.add(item);
+		station.scanner.scan(item);
+		station.scale.add(item);
 		
 		
 		 List<ShoppingCartEntry> cartEntries = cart.getEntries();
-		 Product cartProduct = cartEntries.get(0).getProduct();
-		 assertSame(product, cartProduct);
+		 assertFalse(cartEntries.isEmpty());
 		 
 		 cart.Empty();
+		 scan.emptyStatus();
+		 station.scale.remove(item);
 	
 		
 	}
@@ -86,12 +102,12 @@ public class CustomerScansItemTest {
 		Numeral[] n1 = {Numeral.one, Numeral.two, Numeral.three};
 		Barcode b1 = new Barcode(n1);
 		BarcodedItem item1 = new BarcodedItem(b1, 2.0);
-		scanner.scan(item1);
+		station.scanner.scan(item1);
 		
 		Numeral[] n2 = {Numeral.three, Numeral.two, Numeral.one};
 		Barcode b2 = new Barcode(n2);
 		BarcodedItem item2 = new BarcodedItem(b2, 2.0);
-		scanner.scan(item2);
+		station.scanner.scan(item2);
 		
 		assertEquals(scan.getScanStatus(b2), null);
 		
@@ -100,25 +116,36 @@ public class CustomerScansItemTest {
 	}
 	
 	@Test
-	public void scanDuplicateItems() {     //this tests scanner rather than CustomerScansItem lol
+	public void scanDuplicateProducts() {     //this tests scanner rather than CustomerScansItem lol
 		Numeral[] n1 = {Numeral.one, Numeral.two, Numeral.three};
 		Barcode b1 = new Barcode(n1);
 		BarcodedItem item1 = new BarcodedItem(b1, 2.0);
-		scanner.scan(item1);
-		scale.add(item1);
+		BarcodedProduct product = new BarcodedProduct(b1, "a product", BigDecimal.valueOf(1.99));
+		database.RegisterProducts(product);
 		
-		scanner.scan(item1);
-		scale.add(item1);
+		BarcodedItem item2 = new BarcodedItem(b1, 2.0);
 		
-		assertEquals(cart.getEntries().get(0).getProduct(), cart.getEntries().get(1).getProduct());
+		
+		station.scanner.scan(item1);
+		station.scale.add(item1);
+	
+		station.scale.remove(item1);
+		
+		
+		station.scanner.scan(item2);
+		station.scale.add(item2);
+		
+		station.scale.remove(item2);
+		
+		assertEquals(cart.getEntries().size(), 2);
 		cart.Empty();
 		scan.emptyStatus();
 	}
 	
 	@Test
 	public void scanAfterBlockOveriddenTest() {
-		scanner.disable();
-		scan.overrideBlock(scanner);
+		station.scanner.disable();
+		scan.overrideBlock(station.scanner);
 		
 		Numeral[] n = {Numeral.one, Numeral.two, Numeral.three};
 		Barcode b = new Barcode(n);
@@ -127,16 +154,18 @@ public class CustomerScansItemTest {
 		BarcodedProduct product = new BarcodedProduct(b, "a product", BigDecimal.valueOf(1.99));
 		database.RegisterProducts(product);
 		
-		scanner.scan(item);
-		scale.add(item);
+		station.scanner.scan(item);
+		station.scale.add(item);
 		
 		 List<ShoppingCartEntry> cartEntries = cart.getEntries();
-		 Product cartProduct = cartEntries.get(0).getProduct();
-		 assertSame(product, cartProduct);
+	
+		 assertFalse(cartEntries.isEmpty());
 		
 		 cart.Empty();
+		 station.scale.remove(item);
 	}
 	
+	/*
 	@Test
 	public void itemNotAddedToBaggingAreaTest() {
 		Numeral[] n = {Numeral.one, Numeral.two, Numeral.three};
@@ -146,36 +175,37 @@ public class CustomerScansItemTest {
 		BarcodedProduct product = new BarcodedProduct(b, "a product", BigDecimal.valueOf(1.99));
 		database.RegisterProducts(product);
 		
-		scanner.scan(item);
-		try {
-			TimeUnit.SECONDS.sleep(6);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		assertTrue(scanner.isDisabled());
-		
-		
-	}
+		station.scanner.scan(item);
 	
+		assertTrue(station.scanner.isDisabled());
+		
+		scan.emptyStatus();
+	}
+	*/
+	
+	/*
 	@Test(expected = OverloadException.class)
 	public void currentWeightOverLimitTest() throws OverloadException {
 		ItemStub item = new ItemStub(15.0);
-		scale.add(item);	
-		
+		station.scale.add(item);
+		scan.getItemWeightFromBaggingArea();
+		station.scale.remove(item);
 	}
-	
+	*/
+	/*
 	@Test
 	public void itemWeightCalculatedTest() {
 		ItemStub item = new ItemStub(5.0);
-		scale.add(item);
+		station.scale.add(item);
 		float weight = scan.getItemWeightFromBaggingArea();
 		assertEquals(weight, 5.0f, 0.001);
+		station.scale.remove(item);
 		
 	}
+	*/
 	
 	@Test
-	public void multipleItemWeightCalculatedTest() {
+	public void multipleProductTest() {
 		Numeral[] n = {Numeral.one, Numeral.two, Numeral.three};
 		Barcode b = new Barcode(n);
 		BarcodedItem item = new BarcodedItem(b, 2.0);
@@ -183,16 +213,21 @@ public class CustomerScansItemTest {
 		BarcodedProduct product = new BarcodedProduct(b, "a product", BigDecimal.valueOf(1.99));
 		database.RegisterProducts(product);
 		
-		scale.add(item);
-		scanner.scan(item);
+		station.scanner.scan(item);
 		
 		Numeral[] n1 = {Numeral.two, Numeral.two, Numeral.three};
 		Barcode b1 = new Barcode(n1);
 		BarcodedItem item1 = new BarcodedItem(b1, 3.0);
+
+		BarcodedProduct product1 = new BarcodedProduct(b1, "a product", BigDecimal.valueOf(1.99));
+		database.RegisterProducts(product1);
 		
-		scale.add(item1);
-		assertEquals(scan.getItemWeightFromBaggingArea(), 5.0f, 0.001);
-		
+		station.scanner.scan(item1);
+
+		assertEquals(cart.getEntries().size(), 2);
+	
+		cart.Empty();
+		scan.emptyStatus();
 		
 		
 	}
